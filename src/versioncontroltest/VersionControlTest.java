24e4b76eab5de2cc01e52fc0df0022279041a4e5
Jonas <Jonas@192.168.1.105>
2014-03-05 13:17:20 +0100

    new commit

---
+++ src/versioncontroltest/VersionControlTest.java
@@ -0,0 +1,15 @@
+
+package versioncontroltest;
+
+/**
+ *
+ * @author Jonas
+ */
+public class VersionControlTest
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Jun!");
+    }
+}
